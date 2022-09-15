a455251f8ea4da97e0c71b4e5a58a117937d02d5
Rene Arrieta <renearrieta8905@gmail.com>
2022-09-15 14:26:46 -0500

    Cambios de Clase Semana 5

--- aeropuerto_fnt/src/app/app.component.ts
+++ aeropuerto_fnt/src/app/app.component.ts
@@ -6,31 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  resultado:number = 0;
-  resultado2:number = 0;
-  resultado3:number = 0;
+  lista = [
+    {'marca':'Chevrolet','modelos':[
+        {'linea':'Captiva','version':'Lts'},
+        {'linea':'Spark GT','version':'Lts'},
+        {'linea':'Spark Go','version':'Lts'},
+      ],'Disponibilidad':[
+        {'ciudad':'Medellin'},
+        {'ciudad':'Pasto'},
+        {'ciudad':'Bogota'},
+        {'ciudad':'Cali'},
+      ]
+    },
+    {'marca':'Volkswagen','modelos':[
+        {'linea':'Gol','version':'Trendline'},
+        {'linea':'Jetta','version':'Confortline'},
+        {'linea':'Fox','version':'Trendline'},
+      ],'Disponibilidad':[
+        {'ciudad':'Pereira'},
+        {'ciudad':'Manizales'},
+        {'ciudad':'Bogota'},
+        {'ciudad':'Cali'},
+      ]
+    },
+    {'marca':'Renault','modelos':[
+        {'linea':'Duster','version':'Dynamique'},
+        {'linea':'Sandero','version':'Dynamic'},
+        {'linea':'Logan','version':'Authentique'},
+      ],'Disponibilidad':[
+        {'ciudad':'Medellin'},
+        {'ciudad':'Pasto'},
+        {'ciudad':'Bogota'},
+        {'ciudad':'Cali'},
+      ]
+    },
+    {'marca':'Mazda','modelos':[
+        {'linea':'Mazda 2','version':'Dynamique'},
+        {'linea':'Mazda 3','version':'Dynamic'},
+        {'linea':'CX5','version':'Authentique'},
+      ],'Disponibilidad':[
+        {'ciudad':'Pereira'},
+        {'ciudad':'Manizales'},
+        {'ciudad':'Bogota'},
+        {'ciudad':'Cali'},
+      ]
+    },
 
-  sumar(num1:string,num2:string){
-    if(parseInt(num1)&&parseInt(num2)){
-      this.resultado = parseInt(num1) + parseInt(num2);
-    }else{
-      alert("Por favor digite dor números diferentes a 0. ")
-    }
+  ]
+  marcaSeleccionada: any;
+
+  adicionar_marca(marca:string){
+
   }
 
-  restar(num3:string,num4:string){
-    if(parseInt(num3)&&parseInt(num4)){
-      this.resultado2 = parseInt(num3) - parseInt(num4);
-    }else{
-      alert("Por favor digite dor números diferentes a 0. ")
-    }
-  }
-
-  multiplicar(num5:string,num6:string){
-    if(parseInt(num5)&&parseInt(num6)){
-      this.resultado3 = parseInt(num5) * parseInt(num6);
-    }else{
-      alert("Por favor digite dor números diferentes a 0. ")
-    }
-  }
 }
